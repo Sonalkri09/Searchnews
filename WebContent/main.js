eeee3a102b873caa8bbe10654a723f2a23d1378a
amohan13 <anandmohanca31@gmail.com>
2018-02-28 17:03:50 +0530

    json

--- WebContent/main.js
+++ WebContent/main.js
@@ -1,10 +1,12 @@
 //var url = 'https://newsapi.org/v2/top-headlines?' +
           //'country=us&' +
          // 'apiKey=bb5ba7c8064b42068db9d7cf9219bdad';
+
+var data1="";
 function news(){
 alert ("Hello world!");
 var api = 'https://newsapi.org/v2/top-headlines?';
-//var city = '';
+
 var apikey = '0e8cc5ab21834bab8fe426640b727965';
 var input = document.getElementById('userinput').value;
 console.log(input);
@@ -23,33 +25,41 @@ fetch(req)
 
 let printdata=function(data){
 	 var htmlText = '';
-	var data1 =[];
-    for ( var i=0;i<20;i++ ) {
-   	 //console.log(data.results[i].title);
-        htmlText += '<div class="div-conatiner">';
-        htmlText += '<div class="col_sm_4">';
-        htmlText += '<p class="p-name"> Author: ' + data.articles[i].author + '</p>';
-        htmlText += '<p class="p-loc">Title: ' + data.articles[i].title + '</p>';
-        htmlText += '<p class="p-desc"> Description: ' + data.articles[i].description + '</p>';
-        htmlText += '<p class="p-created"> Publised at: ' + data.articles[i].publishedAt + '</p>';
-        htmlText += '</div>';
-        
-        htmlText += '<div><img src='+data.articles[i].urlToImage+'></div>';
-        htmlText += '<a href='+data.articles[i].url+'>';
-        htmlText += '<input type="button" value="Visit Page" />';
-        htmlText += '</a>';
+   for ( var i=0;i<20;i++ ) {
+  	 console.log(data.articles[i].title);
+       htmlText += '<div class="div-conatiner">';
+       htmlText += '<div class="col_sm_4">';
+       htmlText += '<p class="p-name"> Author: ' + data.articles[i].author + '</p>';
+       htmlText += '<p class="p-loc">Title: ' + data.articles[i].title + '</p>';
+       htmlText += '<p class="p-desc"> Description: ' + data.articles[i].description + '</p>';
+       htmlText += '<p class="p-created"> Publised at: ' + data.articles[i].publishedAt + '</p>';
+       htmlText += '</div>';
        
-        
-       data1[i]=data.articles[i].title;
-        htmlText += '<button onClick=incount(data1,1) >Add to favourites</button>';
-        //htmlText += '<script type="text/javascript" src="function1.js"></script>';
-        htmlText += '<hr>';
-    }
+       htmlText += '<div id="images"><img src='+data.articles[i].urlToImage+' height=200px width=200px></div>';
+       htmlText += '<a href='+data.articles[i].url+'>';
+       htmlText += '<input type="button" value="Visit Page" />';
+       htmlText += '</a>';
+       htmlText += '<button id="addfav" onClick="setData('+data.articles[i].title+')">Add to favourites</button>';
+       //htmlText += '<script type="text/javascript" src="main.js"></script>';
+       htmlText += '<hr>';
+   }
 document.getElementById('result').insertAdjacentHTML('afterend',htmlText);
-}
+};
 
 
-function incount(data1,p){
-	alert("hello");
-	console.log(data1);
-}
+
+
+let setData =function (data1){
+	alert("hi");
+	var xmlhttp = new XMLHttpRequest();
+	
+	xmlhttp.onreadystatechange = function(){
+		if(xmlhttp.readyState == 4 && xmlhttp.status == 200){
+			document.getElementById('fav').innerHTML = xmlhttp.responseText;
+		}
+	};
+	
+	var params = "data="+data1;
+	xmlhttp.open('GET',"http://localhost:8080/newsearch/MyServlet?"+params, true);
+	xmlhttp.send();
+};
---
+++ src/com/news/MyServlet.java
@@ -0,0 +1,57 @@
+package com.news;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		JSONObject info = new JSONObject();
+		JSONArray  addresses = new JSONArray();
+		try {
+			info.put("data",request.getParameter("data"));
+		} catch (JSONException e) {e.printStackTrace();
+		}
+	
+	try {
+		
+		FileWriter jsonFileWriter = new FileWriter("//home//sapient//Documents//workspace-sts-3.9.2.RELEASE//newsearch//src//com//news//dav.json",true);
+		//System.out.println(info.toString());
+		jsonFileWriter.write(info.toString());
+		jsonFileWriter.flush();
+		jsonFileWriter.close();
+		
+	} catch (Exception e) {
+		e.printStackTrace();
+	} finally {
+		response.setContentType("application/json");
+		response.getWriter().write(info.toString());
+	}
+}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
